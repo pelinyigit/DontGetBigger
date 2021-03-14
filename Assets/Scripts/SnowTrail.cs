91a770bd9fd41b3c838f4330a6eec2f84e299b88
ilbara <gungoro@gmail.com>
2021-03-14 11:35:49 +0300

    Fixed Colliders, Added Fire and Snow Trail FX

---
+++ Assets/Scripts/SnowTrail.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+    [SerializeField]Transform player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, 0.1f, player.position.z);
+    }
+}
